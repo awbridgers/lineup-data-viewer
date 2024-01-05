4c5170bd16058e05f58e2a9784ed391626e04969
Adam Bridgers <16521414+awbridgers@users.noreply.github.com>
2024-01-05 18:29:18 -0500

    disables the filter on individual games because it is not really needed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -115,153 +115,166 @@ const Header = ({
     setyearOptions(yearOptions);
   }, [years]);
   return (
-    <div style = {{'paddingBottom': '10px'}}>
-    <HeaderStyle>
-      <div className="headerYearControls">
-        <div className="selectContainer">
-          <Select
-            options={yearOptions}
-            onChange={(picked) =>
-              picked
-                ? changeYear(picked.value)
-                : console.log('no option selected')
-            }
-            isClearable={false}
-            value={yearOptions.find((x) => x.value === selectedYear)}
-            getOptionLabel={(option) => option.label}
-            getOptionValue={(option) => option.value}
-            className="select year"
-            isSearchable={false}
-            isDisabled={finderActive || selectedGroup === 'yearly'}
-            styles={{
-              control: (provided) => ({...provided, padding: '5px 0px'}),
-              valueContainer: (provided) => ({...provided, marginTop: 'auto'}),
-            }}
-            components={{
-              Control: (props) => <Control {...props} title="Season" />,
-            }}
-          />
-        </div>
-        <div className="headerFinder">
-          <button onClick={() => changeShowFinder(true)}>Search Lineups</button>
-        </div>
-      </div>
-      <div className="headerGameControls">
-        {finderActive && (
-          <div
-            className="gameInfo"
-            style={{display: 'flex', flexFlow: 'column'}}
-          >
-            <div className="totals">Lineup Finder</div>
-            <button className="back" onClick={changeFinderActive}>
-              Exit Finder
+    <div style={{paddingBottom: '10px'}}>
+      <HeaderStyle>
+        <div className="headerYearControls">
+          <div className="selectContainer">
+            <Select
+              options={yearOptions}
+              onChange={(picked) =>
+                picked
+                  ? changeYear(picked.value)
+                  : console.log('no option selected')
+              }
+              isClearable={false}
+              value={yearOptions.find((x) => x.value === selectedYear)}
+              getOptionLabel={(option) => option.label}
+              getOptionValue={(option) => option.value}
+              className="select year"
+              isSearchable={false}
+              isDisabled={finderActive || selectedGroup === 'yearly'}
+              styles={{
+                control: (provided) => ({...provided, padding: '5px 0px'}),
+                valueContainer: (provided) => ({
+                  ...provided,
+                  marginTop: 'auto',
+                }),
+              }}
+              components={{
+                Control: (props) => <Control {...props} title="Season" />,
+              }}
+            />
+          </div>
+          <div className="headerFinder">
+            <button onClick={() => changeShowFinder(true)}>
+              Search Lineups
             </button>
           </div>
-        )}
-        {!finderActive && (
-          <div className="gameInfo">
-            {selectedGame === -2 ? (
-              <div className="totals">Season Total</div>
-            ) : selectedGame === -1 ? (
-              <div className="totals">ACC Total</div>
-            ) : (
-              <div className="score">
-                <div>Wake Forest: {games[selectedGame].score.wake}</div>
-                <div>
-                  {games[selectedGame].game}: {games[selectedGame].score.opp}
+        </div>
+        <div className="headerGameControls">
+          {finderActive && (
+            <div
+              className="gameInfo"
+              style={{display: 'flex', flexFlow: 'column'}}
+            >
+              <div className="totals">Lineup Finder</div>
+              <button className="back" onClick={changeFinderActive}>
+                Exit Finder
+              </button>
+            </div>
+          )}
+          {!finderActive && (
+            <div className="gameInfo">
+              {selectedGame === -2 ? (
+                <div className="totals">Season Total</div>
+              ) : selectedGame === -1 ? (
+                <div className="totals">ACC Total</div>
+              ) : (
+                <div className="score">
+                  <div>Wake Forest: {games[selectedGame].score.wake}</div>
+                  <div>
+                    {games[selectedGame].game}: {games[selectedGame].score.opp}
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
+          )}
+
+          <div className="selectContainer">
+            <Select<gameChoice>
+              options={gameOptions}
+              value={gameOptions.find((x) => x.value === selectedGame)}
+              onChange={(picked) =>
+                picked
+                  ? changeGame(picked.value)
+                  : console.log('No option selected')
+              }
+              className="game select"
+              isSearchable={false}
+              isClearable={false}
+              isDisabled={finderActive}
+              getOptionLabel={(option) => option.label}
+              getOptionValue={(option) => option.value.toFixed()}
+              styles={{
+                control: (provided) => ({...provided, padding: '5px 0px'}),
+                valueContainer: (provided) => ({
+                  ...provided,
+                  marginTop: 'auto',
+                }),
+              }}
+              components={{
+                Control: (props) => <Control {...props} title="Game" />,
+              }}
+            />
           </div>
-        )}
-        
-        <div className="selectContainer">
-          <Select<gameChoice>
-            options={gameOptions}
-            value={gameOptions.find((x) => x.value === selectedGame)}
-            onChange={(picked) =>
-              picked
-                ? changeGame(picked.value)
-                : console.log('No option selected')
-            }
-            className="game select"
-            isSearchable={false}
-            isClearable={false}
-            isDisabled={finderActive}
-            getOptionLabel={(option) => option.label}
-            getOptionValue={(option) => option.value.toFixed()}
-            styles={{
-              control: (provided) => ({...provided, padding: '5px 0px'}),
-              valueContainer: (provided) => ({...provided, marginTop: 'auto'}),
-            }}
-            components={{
-              Control: (props) => <Control {...props} title="Game" />,
-            }}
-          />
         </div>
-      </div>
-      <div className="headerStatControls">
-        <div className="selectController">
-          <Select<statChoice>
-            options={statOptions}
-            value={statOptions.find((x) => x.value === selectedStat)}
-            onChange={(picked) =>
-              picked
-                ? changeStat(picked.value)
-                : console.log('No option selected')
-            }
-            className="stat select"
-            isSearchable={false}
-            isClearable={false}
-            getOptionLabel={(option) => option.label}
-            getOptionValue={(option) => option.value}
-            styles={{
-              control: (provided) => ({...provided, padding: '5px 0px'}),
-              valueContainer: (provided) => ({...provided, marginTop: 'auto'}),
-            }}
-            components={{
-              Control: (props) => <Control {...props} title="Stat Type" />,
-            }}
-          />
+        <div className="headerStatControls">
+          <div className="selectController">
+            <Select<statChoice>
+              options={statOptions}
+              value={statOptions.find((x) => x.value === selectedStat)}
+              onChange={(picked) =>
+                picked
+                  ? changeStat(picked.value)
+                  : console.log('No option selected')
+              }
+              className="stat select"
+              isSearchable={false}
+              isClearable={false}
+              getOptionLabel={(option) => option.label}
+              getOptionValue={(option) => option.value}
+              styles={{
+                control: (provided) => ({...provided, padding: '5px 0px'}),
+                valueContainer: (provided) => ({
+                  ...provided,
+                  marginTop: 'auto',
+                }),
+              }}
+              components={{
+                Control: (props) => <Control {...props} title="Stat Type" />,
+              }}
+            />
+          </div>
+          <div className="playerSwitch">
+            <Select<groupChoice>
+              options={groupOptions}
+              value={groupOptions.find((x) => x.value === selectedGroup)}
+              onChange={(picked) =>
+                picked
+                  ? changeGroup(picked.value)
+                  : console.log('No option selected')
+              }
+              className="group select"
+              isSearchable={false}
+              isClearable={false}
+              isDisabled={finderActive}
+              getOptionLabel={(option) => option.label}
+              getOptionValue={(option) => option.value}
+              styles={{
+                control: (provided) => ({...provided, padding: '5px 0px'}),
+                valueContainer: (provided) => ({
+                  ...provided,
+                  marginTop: 'auto',
+                }),
+              }}
+              components={{
+                Control: (props) => <Control {...props} title="Group" />,
+              }}
+            />
+          </div>
         </div>
-        <div className="playerSwitch">
-          <Select<groupChoice>
-            options={groupOptions}
-            value={groupOptions.find((x) => x.value === selectedGroup)}
-            onChange={(picked) =>
-              picked
-                ? changeGroup(picked.value)
-                : console.log('No option selected')
-            }
-            className="group select"
-            isSearchable={false}
-            isClearable={false}
-            isDisabled={finderActive}
-            getOptionLabel={(option) => option.label}
-            getOptionValue={(option) => option.value}
-            styles={{
-              control: (provided) => ({...provided, padding: '5px 0px'}),
-              valueContainer: (provided) => ({...provided, marginTop: 'auto'}),
-            }}
-            components={{
-              Control: (props) => <Control {...props} title="Group" />,
-            }}
-          />
-        </div>
-      </div>
-    </HeaderStyle>
-    {selectedGame < 0 && (
-          <Filter>
-            <label style = {{margin: '0px 5px'}}>Poss Limit</label>
-              <Switch
-                checked={filter}
-                onChange={(checked) => setFilter(checked)}
-                height={20}
-                width = {35}
-                handleDiameter={18}
-              />
-          </Filter>
-        )}
+      </HeaderStyle>
+      <Filter>
+        <label style={{margin: '0px 5px'}}>Poss Limit</label>
+        <Switch
+          checked={filter}
+          onChange={(checked) => setFilter(checked)}
+          height={20}
+          width={35}
+          handleDiameter={18}
+          disabled={selectedGame >= 0}
+        />
+      </Filter>
     </div>
   );
 };
